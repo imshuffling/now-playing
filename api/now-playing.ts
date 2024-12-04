576801d90bb054074acdc792b4e7a1c4ef0a7c38
David Riches <hi@davidrich.es>
2024-12-04 19:37:39 +0000

    latest changes

--- api/now-playing.ts
+++ api/now-playing.ts
@@ -3,16 +3,14 @@ import { renderToString } from "react-dom/server";
 import { decode } from "querystring";
 import { Player } from "../components/NowPlaying";
 import { nowPlaying } from "../utils/spotify";
-import fetch from 'node-fetch';
 
 export default async function (req: NowRequest, res: NowResponse) {
   const {
-    item = {},
+    item = ({} as any),
     is_playing: isPlaying = false,
     progress_ms: progress = 0,
   } = await nowPlaying();
 
-  // @ts-ignore
   const params = decode(req.url.split("?")[1]) as any;
 
   if (params && typeof params.open !== "undefined") {
@@ -34,15 +32,12 @@ export default async function (req: NowRequest, res: NowResponse) {
   const cover = images[images.length - 1]?.url;
   let coverImg = null;
   if (cover) {
-    // @ts-ignore
     const buff = await (await fetch(cover)).arrayBuffer();
     coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString("base64")}`;
   }
 
-  // @ts-ignore
   const artist = (item.artists || []).map(({ name }) => name).join(", ");
   const text = renderToString(
-    // @ts-ignore
     Player({ cover: coverImg, artist, track, isPlaying, progress, duration })
   );
   return res.status(200).send(text);
