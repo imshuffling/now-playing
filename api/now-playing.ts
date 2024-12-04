f7192cfbd5a1bf5cd088897b756586813f9e5e3c
David Riches <hi@davidrich.es>
2024-12-04 19:21:16 +0000

    zzzz ok

--- api/now-playing.ts
+++ api/now-playing.ts
@@ -1,8 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-// import { renderToString } from "react-dom/server";
-
-// @ts-ignore
-import { renderToString } from 'react-dom/server';
+import { renderToString } from "react-dom/server";
 import { decode } from "querystring";
 import { Player } from "../components/NowPlaying";
 import { nowPlaying } from "../utils/spotify";
